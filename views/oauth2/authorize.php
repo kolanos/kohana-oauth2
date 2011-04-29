6041647cbb189fc0025530d952e5c38bd3149975
Michael Lavers <kolanos@gmail.com>
2011-04-28 18:31:23 -0700

    Updating authorize view to not use short tags

--- views/oauth2/authorize.php
+++ views/oauth2/authorize.php
@@ -2,9 +2,9 @@
 	<head>Authorize</head>
 	<body>
 		<form method="post">
-			<? foreach ($oauth_params as $k => $v): ?>
-				<input type="hidden" name="<?=$k ?>" value="<?=$v ?>" />
-			<? endforeach; ?>
+			<?php foreach ($oauth_params as $k => $v): ?>
+				<input type="hidden" name="<?php echo $k ?>" value="<?php echo $v ?>" />
+			<?php endforeach; ?>
 			<p>Do you authorize the app to do its thing?</p>
 			<p>
 				<input type="submit" name="accept" value="yes" />
