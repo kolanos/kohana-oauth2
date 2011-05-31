e6b08d7fefd175ff8f86d8e674211746d407f725
Michael Lavers <kolanos@gmail.com>
2011-05-31 12:13:59 -0700

    Commenting out routes in init

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::set('oauth2_endpoint', 'oauth2/<action>', array(
+/*Route::set('oauth2_endpoint', 'oauth2/<action>', array(
 		'action' => '(authorize|token|return|debug)',
 	))->defaults(array(
 		'controller' => 'oauth2_endpoint',
@@ -9,7 +9,7 @@ Route::set('oauth2_endpoint', 'oauth2/<action>', array(
 Route::set('oauth2_resource', 'oauth2/<controller>(/<action>(/<id>))')
 	->defaults(array(
 		'directory' => 'oauth2',
-	));
+	));*/
 	
 /** 
  * A replacement for apache_request_headers() for Nginx, Lighttpd and other web servers
